38eefd59145b277466dd1ffde2999baf720aa266
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-07 22:05:59 +0000

    modified the eqArray function to inspect nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -16,7 +16,11 @@ const eqArrays = function(arr1, arr2) {
     return false;
   } else {
     for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
+      if (Array.isArray(arr1[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      } else if (arr1[i] !== arr2[i]) {
         return false;
       }
     }
@@ -24,11 +28,39 @@ const eqArrays = function(arr1, arr2) {
   }
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
-assertEqual(eqArrays([], []), true); // => should PASS
-assertEqual(eqArrays([]), false); // => should PASS
+// TESTING
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => pass
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => pass
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => pass
 
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => should PASS
+// assertEqual(eqArrays([], []), true); // => should PASS
+// assertEqual(eqArrays([]), false); // => should PASS
 
+let arr1 = [
+  [
+    [
+      [
+        [
+          2, 3, 4, [5, 6, 7]
+        ]
+      ]
+    ]
+  ]
+];
+
+let arr2 = [
+  [
+    [
+      [
+        [
+          2, 3, 4, [5, 6, 7]
+        ]
+      ]
+    ]
+  ]
+];
+assertEqual(eqArrays(arr1, arr2), true); // => should PASS
