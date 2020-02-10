e275d9d87d5f7205a1588a4037a918f5c2a2292c
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-10 18:04:31 +0000

    refactored code by using exports/require with Andy

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (typeof arr1 !== "object" || typeof arr2 !== "object") {
-    return false;
-  }
-
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
@@ -23,11 +8,7 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [3, 4, 5]); // fail
-assertArraysEqual([1, 2, 3], [3, 4, 5]);  // fail
-assertArraysEqual([1, 2, 3, "4"], [1, 2, 3, 4]);  // fail
-assertArraysEqual([], []); // passed
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // passed
-assertArraysEqual([]); // fail
+module.exports = assertArraysEqual;
+
 
 
