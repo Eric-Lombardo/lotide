714ae9d82c7e159795ff8b64044dc545296836d7
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-10 22:58:06 +0000

    used es6 object property shorthand to refacor my exported object

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,9 +1,9 @@
 const assertEqual = require("../assertEqual")
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("coding", "coding");
-assertEqual(1, 1);
-assertEqual(21, 42);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual("coding", "coding");
+// assertEqual(1, 1);
+// assertEqual(21, 42);
 
 
